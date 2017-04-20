79f06670477a370abde5ee7ec92d6f2095d2a971
Arnaud Jutzeler <arnaud.jutzeler@chuv.ch>
2017-04-20 16:21:47 +0200

    Fixed after-git-clone.sh

--- after-git-clone.sh
+++ after-git-clone.sh
@@ -2,5 +2,5 @@
 git submodule update --init
 
 git fetch sp8 gh-pages
-git worktree add ../specifications.pages sp8/gh-pages
+git worktree add ../deployment.pages sp8/gh-pages
 (cd ../deployment.pages && git checkout gh-pages)
